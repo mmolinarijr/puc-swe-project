36fe51f5f4e276f5c2e7c97fc06754b6fffd383a
Mauricio Molinari <mmolinarijr@gmail.com>
2023-09-13 16:37:41 -0300

    chore: refactored app project

---
+++ client/src/components/HeaderView.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- <v-app-bar> -->
+        <main>
+            Projeto Integrado PUC Minas 
+        </main>
+    <!-- </v-app-bar> -->
+</template>
+
+<script setup lang="ts">
+
+</script>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,28 @@
-import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import 'vuetify/styles';
+import './assets/main.css';
 import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
 const app = createApp(App);
 
 const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    theme: {
+        defaultTheme: localStorage.getItem('theme') ?? 'light',
+    },
     components,
     directives,
 });
