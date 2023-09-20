448be8498e35730697f571b8977a78bbbc648176
Mauricio Molinari <mmolinarijr@gmail.com>
2023-09-20 11:51:56 -0300

    feat: improvments login page

--- client/src/views/login/LoginView.vue
+++ client/src/views/login/LoginView.vue
@@ -20,7 +20,7 @@
                                 aspect-ratio="16/9"
                                 cover
                                 alt="Company Logo"
-                                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+                                src="https://img.freepik.com/premium-vector/making-appointment-with-doctor-flat-concept-vector-illustration_151150-12192.jpg">
                             </v-img>
                         </v-card-title>
 
@@ -31,16 +31,12 @@
                                     class="pb-0"
                                     md="12">
                                     <v-text-field
-                                        v-model="form.email"
-                                        label="E-mail"
+                                        v-model="form.username"
+                                        label="Usuário"
                                         outlined
+                                        :rules="[(v) => !!v || 'Usuário é obrigatório']"
                                         dense
-                                        :rules="[
-                                            (v) => !!v || 'E-mail é obrigatório',
-                                            (v) => /.+@.+\..+/.test(v) || 'E-mail deve ser válido',
-                                        ]"
-                                        required
-                                        type="email">
+                                        required>
                                     </v-text-field>
                                 </v-col>
                                 <v-col
@@ -69,6 +65,7 @@
                                     <v-btn
                                         color="info"
                                         rounded
+                                        :loading="isLoading"
                                         type="submit">
                                         Acessar
                                     </v-btn>
@@ -81,24 +78,66 @@
             <v-spacer></v-spacer>
         </v-row>
     </v-container>
+
+    <alert-view
+        v-model="alert.display"
+        :icon="alert.icon"
+        :color="alert.color"
+        :message="alert.text">
+    </alert-view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import axios from 'axios';
 import AlertView from '@/components/layout/AlertView.vue';
+import Alert from '@/services/Alert';
+
+const api = ref(import.meta.env.VITE_API_URL);
+
+// const alert = new Alert();
+
+const alert = ref(new Alert());
 
 const router = useRouter();
 
+const isLoading = ref(false);
+
 const form = ref({
-    email: '',
+    username: '',
     password: '',
 });
 
-const login = () => {
+const validate = () => {
+    console.log('validate', form.value);
+    if (form.value.username && form.value.password) {
+        return true;
+    }
+};
+
+const login = async () => {
     console.log('login', form.value);
-    router.push('/home');
+    alert.value.hide();
+
+    if (validate()) {
+        isLoading.value = true;
+
+        try {
+            const response = await axios.post(`${api.value}/login`, form.value);
+
+            if (response?.status === 200 && response?.data.message === 'User logged in successfully') {
+                router.push('/home');
+            } else {
+                console.log('01 LOGIN else negative - response', response);
+            }
+        } catch (error) {
+            console.error('Login Error :: ', error);
+
+            alert.value.show('Usuário ou senha inválidos', 'mdi-alert-circle-outline', 'error');
+        } finally {
+            isLoading.value = false;
+        }
+    }
 };
 </script>
-
-<style scoped></style>
